ef43dcae07bb57f72dc90140d71032aa6ff10b3f
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-24 18:16:18 +0200

    ragionamenti in più con Donato

--- palindroma/main.js
+++ palindroma/main.js
@@ -4,24 +4,26 @@
 var parola = prompt('scrivi una parola');
 
 // crea una funzione per scorrere le lettere e verificare l'uguaglianza in entrambi i sensi
-function sono_palindroma (parola) {
-    var lettere = parola.split('').reverse().join("");
+function sono_palindroma (p) {
+    var lettere = p.split('').reverse().join("");
     // se palindroma = true
-    if (lettere === parola) {
-        return true;
-        // altrimenti = false
-    } else {
-        return false;
-    }
+    // if (lettere === parola) {
+    //     return true;
+    //     // altrimenti = false
+    // } else {
+    //     return false;
+    // }
+    return lettere;
 }
 
 // se la corrispondenza è vera stampa messaggio positivo
 var risultato = sono_palindroma(parola);
-    if (risultato == true) {
+    if (risultato === parola) {
         console.log('Complimenti! Hai scritto una parola palindroma!');
 // altrimenti messaggio negativo
  } else {
-     console.log('Mi dispiace, ' + parola + ' non è palindroma ');
+     // console.log('Mi dispiace, ' + parola + ' non è palindroma ');
+     console.log(parola + ' al contrario si legge ' + risultato);
  }
 
 
@@ -33,3 +35,10 @@ var risultato = sono_palindroma(parola);
 // } else {
 //     console.log(parola + ' al contrario si legge ' + lettere);
 // }
+var parola_contrario = '';
+for (var i = parola.length - 1; i >= 0; i--) {
+    console.log(parola[i]);
+    parola_contrario += parola[i];
+}
+
+console.log(parola_contrario);
